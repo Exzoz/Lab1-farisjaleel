0adff513e7484db7101c43eebff7292aae48f1e0
Exzoz <farisgjaleel@gmail.com>
2018-06-05 23:46:31 -0700

    lab1-part2

--- src/main/java/hw/Main.java
+++ src/main/java/hw/Main.java
@@ -5,10 +5,32 @@ import java.util.Arrays;
 public class Main {
 
   public static void main(final String[] args) {
-    System.out.println("args = " + Arrays.asList(args));
-    final HelloWorld instance = new HelloWorld();
-    System.out.println(instance.getMessage());
-    System.out.println(instance.getYear());
-    System.out.println("bye for now");
+
+    int max = 0;
+    if (args.length > 0){
+      try {
+        max = Integer.parseInt(args[0]);
+        }
+       catch (NumberFormatException e) {
+        System.out.println("The argument entered must be an integer ");
+        System.exit(1);
+      }
+    }
+
+    if ( max > 0 ) {
+      for (int n = 1; n <= max; n++) {
+        if (n % 15 == 0) {
+          System.out.println("FizzBuzz");
+        } else if (n % 5 == 0) {
+          System.out.println("Buzz");
+        } else if (n % 3 == 0) {
+          System.out.println("Fizz");
+        } else {
+          System.out.println(n);
+        }
+      }
+    } else {
+          System.out.println("The argument entered must be a positive integer ");
+      }
+    }
   }
-}
---
+++ src/test/java/hw/TestFizzBuzz.java
@@ -0,0 +1,14 @@
+ package hw;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class TestFizzBuzz {
+
+
+  }
+
+
