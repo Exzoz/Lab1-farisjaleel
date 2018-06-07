c34077931d7b941ad147e29c18e1519aadc93912
Exzoz <farisgjaleel@gmail.com>
2018-06-06 19:00:23 -0700

    lab1-part3final

--- src/main/java/hw/Main.java
+++ src/main/java/hw/Main.java
@@ -7,30 +7,25 @@ public class Main {
   public static void main(final String[] args) {
 
     int max = 0;
-    if (args.length > 0){
+    if (args.length > 0) {
       try {
         max = Integer.parseInt(args[0]);
-        }
-       catch (NumberFormatException e) {
+      } catch (NumberFormatException e) {
+
         System.out.println("The argument entered must be an integer ");
         System.exit(1);
+
       }
     }
 
-    if ( max > 0 ) {
-      for (int n = 1; n <= max; n++) {
-        if (n % 15 == 0) {
-          System.out.println("FizzBuzz");
-        } else if (n % 5 == 0) {
-          System.out.println("Buzz");
-        } else if (n % 3 == 0) {
-          System.out.println("Fizz");
-        } else {
-          System.out.println(n);
-        }
+    if (max > 0) {
+      FizzBuzz fizzBuzz = new FizzBuzz();
+      String[] result = fizzBuzz.fizzBuzzArray(max);
+      for (int i = 0; i < max; i++) {
+        System.out.println(result[i]);
       }
     } else {
-          System.out.println("The argument entered must be a positive integer ");
-      }
+      System.out.println("Please enter a positive integer");
     }
   }
+}
--- src/test/java/hw/FizzBuzzTest.java
+++ src/test/java/hw/FizzBuzzTest.java
@@ -1,5 +1,44 @@
-import static org.junit.Assert.*;
+package hw;
+
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+import org.junit.Before;
 
 public class FizzBuzzTest {
 
+    private FizzBuzz fixture;
+
+    @Before
+    public void setUp() throws Exception {
+        fixture = new FizzBuzz();
+    }
+
+    @Test
+    public void testFizzBuzz() {
+        //tests inputs
+
+        String[] result = fixture.fizzBuzzArray(-10);
+        assertEquals(null, result);
+
+        result = fixture.fizzBuzzArray(-1);
+        assertEquals(null, result);
+
+        result = fixture.fizzBuzzArray(0);
+        assertEquals(null, result);
+
+        result = fixture.fizzBuzzArray(1);
+        assertEquals(new String[] {"1"}, result);
+
+        result = fixture.fizzBuzzArray(7);
+        assertEquals(new String[] {"1", "2", "Fizz", "4", "Buzz", "Fizz", "7"}, result);
+
+
+        result = fixture.fizzBuzzArray(17);
+        assertEquals(new String[] {"1", "2", "Fizz", "4", "Buzz", "Fizz", "7", "8", "Fizz", "Buzz", "11", "Fizz", "13", "14", "FizzBuzz", "16", "17"}, result);
+
+
+
+
+
+    }
 }
